713b4b9419e5189f9e45564eb740581bf41e99ee
Palash Jain <palash.j22@gmail.com>
2018-07-21 14:33:32 +0530

    Update knn_test_using_iris_dataset.py

--- KNearestNeighbors/knn_test_using_iris_dataset.py
+++ KNearestNeighbors/knn_test_using_iris_dataset.py
@@ -7,9 +7,9 @@ df = pd.read_csv('iris.txt')
 X = df.iloc[:,[0,1,2,3]].values
 y = df.iloc[:,[4]].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
  
 clf = KNN(k=3)
 clf.fit(X_train, y_train)
-for  i in X_test:
-    print(i, clf.predict(i))
+predictions = clf.predict(X_test)
+print(clf.accuracy(y_test, predictions))
