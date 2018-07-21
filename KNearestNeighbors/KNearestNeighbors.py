5e2e28308a1b9a3c2fd9468d602e477b1af241ef
Palash Jain <palash.j22@gmail.com>
2018-07-21 14:34:11 +0530

    Update KNearestNeighbors.py

--- KNearestNeighbors/KNearestNeighbors.py
+++ KNearestNeighbors/KNearestNeighbors.py
@@ -16,15 +16,26 @@ class KNN:
         
         
     def predict(self, test_features):
-        # get the index of all nearest neighbouring data points
-        neighbors = self.__get_neighbours__(self.features, test_features)
-        classVotes = {}
-        # to count votes for each class initialise all class with zero votes
-        for label in np.unique(self.labels):
-             classVotes[label] = 0
-        # add count to class that are present in the nearest neighbors data points
-        for index in neighbors:
-            closest_lable = self.labels[index]
-            classVotes[closest_lable[0]] += 1
+        results = []
+        for test_feature in test_features:
+            # get the index of all nearest neighbouring data points
+            neighbors = self.__get_neighbours__(self.features, test_feature)
+            classVotes = {}
+            # to count votes for each class initialise all class with zero votes
+            for label in np.unique(self.labels):
+                 classVotes[label] = 0
+            # add count to class that are present in the nearest neighbors data points
+            for index in neighbors:
+                closest_lable = self.labels[index]
+                classVotes[closest_lable[0]] += 1
+            results.append(max(classVotes.items(), key = operator.itemgetter(1))[0])
 
-        return max(classVotes.items(), key = operator.itemgetter(1))[0]
+        return results
+    
+    def accuracy(self, y_test, predictions):
+        correct = 0
+        for i in range(len(y_test)):
+            if(y_test[i] == predictions[i]):
+                correct += 1
+                
+        return correct/len(y_test)
